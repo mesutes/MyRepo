baa5887bc9a8b0b33ed883729580e0fb66b1ea48
Legomesut <legomesut@gmail.com>
2020-09-03 15:31:32 +0300

    ilk adım atıldı.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace MyRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Araba arb =new Araba();
+           arb.GazaBas();
         }
     }
 }
